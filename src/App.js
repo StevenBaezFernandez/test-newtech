730b99b629ad397df0cfcbbef0d712066bb0131e
Steven <stevenbaezfernandez@gmail.com>
2022-07-18 18:02:44 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const URL = "http://universities.hipolabs.com/search?country=Dominican+Republic";
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const [searchField, setSearchField] = useState('');
+  const [filteredUni, setFilterUni] = useState(data);
+
+
+
+  const handlerChange = (e) => {
+    setSearchField(e.target.value);
+  }
+  
+  useEffect(()=>{
+    fetch(URL)
+    .then(resp => resp.json())
+    .then(data => {
+      setData(data);
+    });
+    
+  }, []);
+
+  useEffect(() => {
+    const newFilteredUni = data.filter((uni) => {
+      return uni.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilterUni(newFilteredUni);
+  });
+
+  console.log(data);
+  console.log(searchField);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+    <input type="search" onChange={handlerChange} placeholder='buscar'/>
+
+    <div className="container">
+        {filteredUni ?
+          filteredUni.map((uni, idx) => {
+            return(
+              <div className="uni" key={idx}>
+                <img src={`${'https://countryflagsapi.com/png/'+ uni.alpha_two_code}`}/>
+                <h2>{uni.name}</h2>
+                <p>{uni.country}</p>
+              </div>
+            )
+          })
+        :null
+        }
+    </div>
     </div>
   );
 }
